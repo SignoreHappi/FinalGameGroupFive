d11b1c3e20d6a470680c1908bfc7bf745711ef5d
Giovane Francisco Forlenza <Gfranciscoforle1239@conestogac.on.ca>
2020-11-25 17:58:16 -0300

    Player class created, empty

---
+++ FinalGameGroupFive/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FinalGameGroupFive
+{
+    class Player : DrawableGameComponent
+    {
+        public Player(Microsoft.Xna.Framework.Game game) : base(game)
+        {
+        }
+    }
+}
