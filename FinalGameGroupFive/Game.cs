95f7cff7f25faa420e3dbcacd433604e34f41227
Giovane Francisco Forlenza <Gfranciscoforle1239@conestogac.on.ca>
2020-11-25 17:52:25 -0300

    Giovane Test

--- FinalGameGroupFive/Game.cs
+++ FinalGameGroupFive/Game.cs
@@ -17,6 +17,7 @@ namespace FinalGameGroupFive
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            //This is my comment
         }
 
         /// <summary>
